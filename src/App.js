16f52114dbeff3906f50faee714abcfcd845c5bc
HoagVu_dev <hoangk62uet@gmail.com>
2022-05-17 23:06:52 +0700

    update eslint rule

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +19,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
