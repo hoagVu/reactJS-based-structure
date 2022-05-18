37e736625656e126bf65f28a6574a8eede94c3f2
Vũ Việt Hoàng <hoangk62uet@gmail.com>
2022-05-18 16:06:18 +0700

    update global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import { Button, Typography } from "@mui/material";
 import { FormattedMessage } from "react-intl";
-import "./assets/styles/App.css";
 
 const App = () => {
   return (
-    <div className="App">
+    <>
       <Button variant="contained">Hello World</Button>
       <Typography variant="body2">
         h1. Heading
@@ -13,7 +12,7 @@ const App = () => {
       <Typography variant="body2">
         <FormattedMessage id="IDS_CHAT_EVALUATE_SERVICE_QUESTION_2" />
       </Typography>
-    </div>
+    </>
   );
 };
 
