7ee3b92e85a427fe58dfffd10146e08ceca1d318
Vũ Việt Hoàng <hoangk62uet@gmail.com>
2022-05-19 09:41:49 +0700

    add utilts files

---
+++ refactor/src/utils/helpers.tsx
@@ -0,0 +1,15 @@
+export type some = { [key: string]: any };
+
+const has = Object.prototype.hasOwnProperty;
+
+export const isDiff = (A: some, B: some) =>
+  JSON.stringify(A) !== JSON.stringify(B);
+
+export const isEmpty = (prop: any) => {
+  return (
+    prop === null ||
+    prop === undefined ||
+    (has.call(prop, "length") && prop.length === 0) ||
+    (prop.constructor === Object && Object.keys(prop).length === 0)
+  );
+};
---
+++ refactor/src/utils/moment.tsx
@@ -0,0 +1,16 @@
+export const TIME_FORMAT = "HH:mm:ss";
+export const HOUR_MINUTE = "HH:mm";
+export const DATE_FORMAT = "DD/MM/YYYY";
+export const DATE_FORMAT_BACK_END = "DD-MM-YYYY";
+export const C_DATE_FORMAT = "YYYY-MM-DD";
+export const DATE_FORMAT_CHART = "MM-DD-YYYY";
+export const DAY_FORMAT = "dddd";
+export const YEAR_FORMAT = "YYYY";
+export const DATE_TIME_FORMAT = `${DATE_FORMAT_BACK_END} ${TIME_FORMAT}`;
+export const DATE_TIME_FORMAT_BREAK = `${DATE_FORMAT}[\n]${TIME_FORMAT}`;
+export const DATE_FORMAT_ALL = `${DATE_FORMAT} ${TIME_FORMAT}`;
+export const DATE_TIME_ISOLATE_FORMAT = `${DATE_FORMAT_BACK_END}, ${TIME_FORMAT}`;
+export const DATE_FORMAT_TIME_BACK_END = `${DATE_FORMAT_BACK_END} ${TIME_FORMAT}`;
+export const DATE_FORMAT_ALL_ISOLATE = `${DATE_FORMAT}, ${TIME_FORMAT}`;
+export const DAY_DATE_ISOLATE = `${DAY_FORMAT}, ${DATE_FORMAT}`;
+export const TIME_DATE_FORMAT = `${TIME_FORMAT} ${DATE_FORMAT}`;
