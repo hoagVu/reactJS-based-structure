10741cf733aba6efff2696025d010d913e9d7470
Vũ Việt Hoàng <hoangk62uet@gmail.com>
2022-05-20 16:40:34 +0700

    hot fix

--- src/components/alert/Alert.tsx
+++ src/components/alert/Alert.tsx
@@ -3,20 +3,17 @@ import * as React from "react";
 
 export default function snackbarSetting(closeSnackbar, alertProps, alertTitle) {
   return {
-    anchorOrigin: {
-      vertical: "top",
-      horizontal: "center",
-    },
+    anchorOrigin: { vertical: "top", horizontal: "center" },
     preventDuplicate: true,
     autoHideDuration: 3000,
     content: (key, msg) => (
       <Alert
-        style={{ minWidth: "240px", display: "flex", alignItems: "center" }}
+        style={{ minWidth: 240, display: "flex", alignItems: "center" }}
         onClose={() => closeSnackbar(key)}
         severity={alertProps?.color}
         {...alertProps}>
         {alertTitle && <AlertTitle>{alertTitle}</AlertTitle>}
-        <Typography variant="body2" color="inherit">
+        <Typography variant="body2" color="inherit" component="span">
           {msg}
         </Typography>
       </Alert>
