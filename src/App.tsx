9479f362e61b38fdc437b48579ab34cf4fb2c16e
Vũ Việt Hoàng <hoangk62uet@gmail.com>
2022-05-24 11:25:08 +0700

    test route Objects example

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { makeStyles } from "@material-ui/styles";
 import "assets/styles/App.scss";
 import MainLayout from "layouts/desktop/MainLayout";
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useRoutes } from "react-router-dom";
 import About from "views/About";
 import Home from "views/Home";
 import NotSupported from "views/NotSupported";
@@ -18,14 +18,29 @@ interface IAppProps {}
 
 const App: React.FunctionComponent<IAppProps> = (props) => {
   const classes = useStyles();
+  const element = useRoutes([
+    // These are the same as the props you provide to <Route>
+    { path: "/", element: <Home /> },
+    // {
+    //   path: "/cryptocurrencies",
+    //   element: <Cryptocurrencies />,
+    //   // Nested routes use a children property
+    //   children: [{ path: ":coinId", element: <CryptoDetails /> }],
+    // },
+    {
+      path: "/about",
+      element: <About />,
+    },
+  ]);
   return (
     <div className={classes.root}>
       <img src={logo} alt="logo" style={{ width: 160, height: 160 }} />
       <NotSupported />
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
-      </Routes>
+      </Routes> */}
+      {element}
     </div>
   );
 };
