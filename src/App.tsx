86362279580278f28bee54e400ca334317d7b9bf
Vũ Việt Hoàng <hoangk62uet@gmail.com>
2022-05-25 14:32:22 +0700

    roll back old code

--- src/App.tsx
+++ src/App.tsx
@@ -22,63 +22,13 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-interface AuthContextType {
-  user: any;
-  signin: (user: string, callback: VoidFunction) => void;
-  signout: (callback: VoidFunction) => void;
-}
-
-const AuthContext = React.createContext<AuthContextType>(null!);
-
-function useAuth() {
-  return React.useContext(AuthContext);
-}
-
-function AuthProvider({ children }: { children: React.ReactNode }) {
-  const [user, setUser] = React.useState<any>(null);
-
-  const signin = (newUser: string, callback: VoidFunction) => {
-    return fakeAuthProvider.signin(() => {
-      setUser(newUser);
-      callback();
-    });
-  };
-
-  const signout = (callback: VoidFunction) => {
-    return fakeAuthProvider.signout(() => {
-      setUser(null);
-      callback();
-    });
-  };
-
-  // eslint-disable-next-line react/jsx-no-constructed-context-values
-  const value = { user, signin, signout };
-
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-}
-
-function RequireAuth({ children }: { children: JSX.Element }) {
-  const auth = useAuth();
-  const location = useLocation();
-
-  if (!auth.user) {
-    // Redirect them to the /login page, but save the current location they were
-    // trying to go to when they were redirected. This allows us to send them
-    // along to that page after they login, which is a nicer user experience
-    // than dropping them off on the home page.
-    return <Navigate to="/login" state={{ from: location }} replace />;
-  }
-
-  return children;
-}
-
 interface IAppProps {}
 
 const App: React.FunctionComponent<IAppProps> = (props) => {
   const classes = useStyles();
 
   return (
-    <AuthProvider>
+    <>
       <h1>Auth Example</h1>
 
       <p>
@@ -91,17 +41,10 @@ const App: React.FunctionComponent<IAppProps> = (props) => {
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/protected"
-            element={
-              <RequireAuth>
-                <About />
-              </RequireAuth>
-            }
-          />
+          <Route path="/protected" element={<About />} />
         </Route>
       </Routes>
-    </AuthProvider>
+    </>
   );
 };
 
