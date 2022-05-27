58240c4efa8a0a7d781949978b28aa31288dc554
Vũ Việt Hoàng <hoangk62uet@gmail.com>
2022-05-27 13:57:55 +0700

    remove code

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,14 @@
-import { makeStyles } from "@material-ui/styles";
 import "assets/styles/App.scss";
 import MainLayout from "layouts/desktop/MainLayout";
 import React from "react";
-import {
-  Navigate,
-  Route,
-  Routes,
-  useLocation,
-  useRoutes,
-} from "react-router-dom";
-import { fakeAuthProvider } from "utils/auth";
+import { Route, Routes } from "react-router-dom";
 import About from "views/About";
 import Home from "views/Home";
 import LoginPage from "views/LoginPage";
-import NotSupported from "views/NotSupported";
-import logo from "./logo.svg";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: "white",
-  },
-}));
 
 interface IAppProps {}
 
 const App: React.FunctionComponent<IAppProps> = (props) => {
-  const classes = useStyles();
-
   return (
     <>
       <Routes>
