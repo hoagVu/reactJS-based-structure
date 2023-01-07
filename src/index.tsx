16c4141ce88ae8b307534b1d68ad339e494dfbeb
hoangVV.dev <hoangk62uet@gmail.com>
2023-01-08 00:01:01 +0700

    down grade react from 18 to 17

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,12 @@ import { ThemeProvider as MuiThemeProvider } from "@material-ui/styles";
 import "assets/styles/global.scss";
 import { MUI_THEME } from "assets/theme/setupTheme";
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import LanguagesProvider from "utils/intl/components/LanguagesProvider";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <MuiThemeProvider theme={MUI_THEME}>
@@ -20,10 +16,6 @@ root.render(
         </LanguagesProvider>
       </MuiThemeProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
